f892b288605b864df872a427de50303c17f85367
Artiom Kalugin <a.o.kalugin@protonmail.com>
2020-12-25 14:03:43 +0000

    Added views directory.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const homeRouter = require("./routes/home");
-const userRouter = require("./routes/user");
+const userRouter = require("./routes/users");
 
 app.use("/", homeRouter);
 app.use("/users", userRouter);
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("<h1>Home</h1>");
+    res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
 module.exports = router;
