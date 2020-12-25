443e7c48ec2dd0ea115c268f38484310fc62e882
Artiom Kalugin <a.o.kalugin@protonmail.com>
2020-12-25 13:55:32 +0000

    Moved routes to separate directory.

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use("/users", (req, res, next) => {
-    console.log("Route displaying list of users.");
-    res.send("<h1>List of Users</h1>");
-});
+const homeRouter = require("./routes/home");
+const userRouter = require("./routes/user");
 
-app.use("/", (req, res, next) => {
-    console.log("Home page route.");
-    res.send("<h1>Hope Page</h1>");
-});
+app.use("/", homeRouter);
+app.use("/users", userRouter);
 
 app.listen(3000);
